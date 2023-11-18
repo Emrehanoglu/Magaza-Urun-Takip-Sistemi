3ed3f408a931e065ae1b29f1791ca0fe52c87b41
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-18 11:35:38 +0300

    MagazaUrunTakipSistemi #5

--- MagazaUrunTakipSistemi/Controllers/MusteriController.cs
+++ MagazaUrunTakipSistemi/Controllers/MusteriController.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
 
 namespace MagazaUrunTakipSistemi.Controllers
 {
     public class MusteriController : Controller
     {
 		DbMagazaUrunTakipEntities db = new DbMagazaUrunTakipEntities();
-		public ActionResult Index()
+		public ActionResult Index(int sayfa=1) //baslangıc sayfam 1 olsun
         {
-			var musteriler = db.TblMusteri.ToList();
+			var musteriler = db.TblMusteri.ToList().ToPagedList(sayfa,3); //1. sayfadan basla ve her sayfada 3 tane veri göster
 			return View(musteriler);
         }
     }
