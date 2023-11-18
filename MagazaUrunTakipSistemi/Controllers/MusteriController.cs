6fcee40a1c2a9d2c44618b9cb2b25aba2e94afcf
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-18 11:23:26 +0300

    MagazaUrunTakipSistemi #4

---
+++ MagazaUrunTakipSistemi/Controllers/MusteriController.cs
@@ -0,0 +1,19 @@
+﻿using MagazaUrunTakipSistemi.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MagazaUrunTakipSistemi.Controllers
+{
+    public class MusteriController : Controller
+    {
+		DbMagazaUrunTakipEntities db = new DbMagazaUrunTakipEntities();
+		public ActionResult Index()
+        {
+			var musteriler = db.TblMusteri.ToList();
+			return View(musteriler);
+        }
+    }
+}
