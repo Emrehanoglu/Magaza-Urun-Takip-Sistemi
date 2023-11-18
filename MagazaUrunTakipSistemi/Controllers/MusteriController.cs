ae1f542d4634613dde5650a30b7897a1727e0c40
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-18 11:53:20 +0300

    MagazaUrunTakipSistemi #6

--- MagazaUrunTakipSistemi/Controllers/MusteriController.cs
+++ MagazaUrunTakipSistemi/Controllers/MusteriController.cs
@@ -17,5 +17,41 @@ namespace MagazaUrunTakipSistemi.Controllers
 			var musteriler = db.TblMusteri.ToList().ToPagedList(sayfa,3); //1. sayfadan basla ve her sayfada 3 tane veri göster
 			return View(musteriler);
         }
-    }
+		[HttpGet]
+		public ActionResult MusteriEkle()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult MusteriEkle(TblMusteri t)
+		{
+			db.TblMusteri.Add(t);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public ActionResult MusteriSil(int id)
+		{
+			var k = db.TblMusteri.Find(id);
+			db.TblMusteri.Remove(k);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public ActionResult MusteriEdit(int id)
+		{
+			var k = db.TblMusteri.Find(id);
+			return View(k);
+		}
+		[HttpPost]
+		public ActionResult MusteriEdit(TblMusteri t)
+		{
+			var k = db.TblMusteri.Find(t.Id);
+			k.Ad = t.Ad;
+			k.Soyad = t.Soyad;
+			k.Sehir = t.Sehir;
+			k.Bakiye = t.Bakiye;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
