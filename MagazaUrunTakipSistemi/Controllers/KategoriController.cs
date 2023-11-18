3f927c18355d8f46966da8912eaa9d42b6309f42
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-18 11:14:40 +0300

    MagazaUrunTakipSistemi #3

--- MagazaUrunTakipSistemi/Controllers/KategoriController.cs
+++ MagazaUrunTakipSistemi/Controllers/KategoriController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MagazaUrunTakipSistemi.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,44 @@ namespace MagazaUrunTakipSistemi.Controllers
 {
     public class KategoriController : Controller
     {
-        // GET: Kategori
+		DbMagazaUrunTakipEntities db = new DbMagazaUrunTakipEntities();
         public ActionResult Index()
         {
-            return View();
+			var kategoriler = db.TblKategori.ToList();
+            return View(kategoriler);
         }
-    }
+		[HttpGet]
+		public ActionResult KategoriEkle()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult KategoriEkle(TblKategori t)
+		{
+			db.TblKategori.Add(t);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public ActionResult KategoriSil(int id)
+		{
+			var k = db.TblKategori.Find(id);
+			db.TblKategori.Remove(k);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public ActionResult KategoriEdit(int id)
+		{
+			var k = db.TblKategori.Find(id);
+			return View(k);
+		}
+		[HttpPost]
+		public ActionResult KategoriEdit(TblKategori t)
+		{
+			var k = db.TblKategori.Find(t.Id);
+			k.Ad = t.Ad;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
---
+++ MagazaUrunTakipSistemi/Controllers/UrunController.cs
@@ -0,0 +1,72 @@
+﻿using MagazaUrunTakipSistemi.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MagazaUrunTakipSistemi.Controllers
+{
+    public class UrunController : Controller
+    {
+		DbMagazaUrunTakipEntities db = new DbMagazaUrunTakipEntities();
+		public ActionResult Index()
+        {
+			var urunler = db.TblUrunler.Where(x=>x.Durum == true).ToList();
+            return View(urunler);
+        }
+		[HttpGet]
+		public ActionResult UrunEkle()
+		{
+			List<SelectListItem> kategoriler = (from x in db.TblKategori
+													   select new SelectListItem
+													   {
+														   Text = x.Ad,
+														   Value = x.Id.ToString()
+													   }).ToList();
+			ViewBag.ktgr = kategoriler;
+			return View();
+		}
+		[HttpPost]
+		public ActionResult UrunEkle(TblUrunler t)
+		{
+			t.Durum = true;
+			db.TblUrunler.Add(t);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public ActionResult UrunSil(int id)
+		{
+			var k = db.TblUrunler.Find(id);
+			k.Durum = false;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public ActionResult UrunEdit(int id)
+		{
+			List<SelectListItem> kategoriler = (from x in db.TblKategori
+												select new SelectListItem
+												{
+													Text = x.Ad,
+													Value = x.Id.ToString()
+												}).ToList();
+			ViewBag.ktgr = kategoriler;
+			var k = db.TblUrunler.Find(id);
+			return View(k);
+		}
+		[HttpPost]
+		public ActionResult UrunEdit(TblUrunler t)
+		{
+			var k = db.TblUrunler.Find(t.Id);
+			k.Ad = t.Ad;
+			k.Marka = t.Marka;
+			k.Stok = t.Stok;
+			k.AlisFiyat = t.AlisFiyat;
+			k.SatisFiyat = t.SatisFiyat;
+			k.Kategori = t.Kategori;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
